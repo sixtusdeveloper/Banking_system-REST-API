6d6e57c44996f6558d5d8d5feb2226172554178d
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-17 18:48:31 +0100

    First commit

---
+++ src/test/java/net/sixtusdev/banking/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sixtusdev.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
